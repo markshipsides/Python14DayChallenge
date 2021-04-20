eac31da4a7a2001322330b42bbf51ff0200e377c
MarkSLegion <markshipsides@yahoo.co.uk>
2021-04-20 12:14:11 +0100

    Version 02

--- PythonDay02.py
+++ PythonDay02.py
@@ -1,15 +1,17 @@
 # MarkS - Python Challenge Day 02 - Logic
 
 CALENDAR_MONTHS: tuple = ("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
-new_superpower: str
+BASIC_SUPERPOWERS: tuple = ("Flight", "Speed")
+
 character_name: str = "Jimmy The Snowman"
 character_sex: str = "Male"
 year_of_birth: int = 1945
 month_of_birth: int = 4
 day_of_birth: int = 1
 character_alive: bool = True
-BASIC_SUPERPOWERS: tuple = ("Flight", "Speed")
+
 extended_superpowers: list = list(BASIC_SUPERPOWERS)
+new_superpower: str
 
 print("Character's Name:", character_name)
 print("Character Born:", day_of_birth, CALENDAR_MONTHS[month_of_birth], year_of_birth)
