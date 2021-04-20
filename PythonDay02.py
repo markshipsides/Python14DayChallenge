ef647de2b1822d598db365ecfb22cf1048fcd7df
MarkSLegion <markshipsides@yahoo.co.uk>
2021-04-20 12:08:23 +0100

    Version 02

--- PythonDay02.py
+++ PythonDay02.py
@@ -1,6 +1,7 @@
 # MarkS - Python Challenge Day 02 - Logic
 
 CALENDAR_MONTHS: tuple = ("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+new_superpower: str
 character_name: str = "Jimmy The Snowman"
 character_sex: str = "Male"
 year_of_birth: int = 1945
@@ -10,11 +11,14 @@ character_alive: bool = True
 BASIC_SUPERPOWERS: tuple = ("Flight", "Speed")
 extended_superpowers: list = list(BASIC_SUPERPOWERS)
 
-
 print("Character's Name:", character_name)
 print("Character Born:", day_of_birth, CALENDAR_MONTHS[month_of_birth], year_of_birth)
-print("Hello")
-if character_alive:
-    print("Character Is Alive")
+print(BASIC_SUPERPOWERS[:])
+new_superpower = input("Please add a Superpower: ")
+
+if new_superpower in extended_superpowers:
+    print("Sorry " + new_superpower + " already exists")
 else:
-    print("Character Is Dead")
+    extended_superpowers.append(new_superpower)
+
+print("Full list of superpowers", extended_superpowers[::-1])
