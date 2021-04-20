392af32a1ff18c692ba44c01272cad7cef76b12e
MarkSLegion <markshipsides@yahoo.co.uk>
2021-04-20 11:25:38 +0100

    Version 01

--- PythonDay02.py
+++ PythonDay02.py
@@ -1,18 +1,19 @@
 # MarkS - Python Challenge Day 02 - Logic
 
-CALENDAR_MONTHS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-character_name = "Jimmy The Snowman"
-year_of_birth = 1945
-month_of_birth = 4
-day_of_birth = 1
-date_of_birth = (year_of_birth, month_of_birth, day_of_birth)
+CALENDAR_MONTHS: tuple = ("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+character_name: str = "Jimmy The Snowman"
+character_sex: str = "Male"
+year_of_birth: int = 1945
+month_of_birth: int = 4
+day_of_birth: int = 1
+character_alive: bool = True
+BASIC_SUPERPOWERS: tuple = ("Flight", "Speed")
+extended_superpowers: list = list(BASIC_SUPERPOWERS)
 
-character_sex = "Male"
-character_alive = True
 
 print("Character's Name:", character_name)
 print("Character Born:", day_of_birth, CALENDAR_MONTHS[month_of_birth], year_of_birth)
-
+print("Hello")
 if character_alive:
     print("Character Is Alive")
 else:
