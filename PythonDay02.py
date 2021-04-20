4002a7856d4f0f461005624ab15b5108baa23f7b
MarkSLegion <markshipsides@yahoo.co.uk>
2021-04-20 12:30:01 +0100

    Version 02

--- PythonDay02.py
+++ PythonDay02.py
@@ -10,18 +10,12 @@ month_of_birth: int = 4
 day_of_birth: int = 1
 character_alive: bool = True
 
-extended_superpowers: list = list(BASIC_SUPERPOWERS)
+extended_superpowers: set = set(BASIC_SUPERPOWERS)
 new_superpower: str
 
 print("Character's Name:", character_name)
 print("Character Born:", day_of_birth, CALENDAR_MONTHS[month_of_birth], year_of_birth)
 
 new_superpower = input("Please add a Superpower: ")
-
-if new_superpower in extended_superpowers:
-    print("Sorry " + new_superpower + " already exists")
-else:
-    extended_superpowers.append(new_superpower)
-    print("Thank you, " + new_superpower + " added")
-
-print("Full list of Superpowers ", extended_superpowers[::-1])
+extended_superpowers.add(new_superpower)
+print("Full list of Superpowers ", extended_superpowers)
