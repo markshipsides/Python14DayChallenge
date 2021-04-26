fa8d3a77211c4efad4c0df5a25b3742f28eb229d
MarkSLegion <markshipsides@yahoo.co.uk>
2021-04-26 15:31:56 +0100

    Version 02

---
+++ PythonDay03.py
@@ -0,0 +1,40 @@
+# MarkS - Python Challenge Day 02 - Logic
+
+CALENDAR_MONTHS: tuple = ("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+BASIC_SUPERPOWERS: dict = {"Flight": 51, "Speed": 90}
+WALLET: dict = {"USD": 170, "GBP": 50, "EUR": 30, "YEN": 50}
+SUPERPOWER_UPPER_LIMIT = 80
+SUPERPOWER_LOWER_LIMIT = 50
+
+
+character_name: str = "Jimmy The Snowman"
+character_sex: str = "Male"
+year_of_birth: int = 1945
+month_of_birth: int = 4
+day_of_birth: int = 1
+character_alive: bool = True
+
+extended_superpowers = BASIC_SUPERPOWERS
+new_superpower: str
+new_superpower_rating: int
+superpower_test: str
+superpower_test_value: int
+
+print("Character's Name:", character_name)
+print("Character Born:", day_of_birth, CALENDAR_MONTHS[month_of_birth], year_of_birth)
+print("Basic superpowers are: ", BASIC_SUPERPOWERS)
+
+new_superpower = input("Please add a Superpower: ")
+new_superpower_rating = int(input("Please add a Superpower Rating: "))
+extended_superpowers[new_superpower] = new_superpower_rating
+
+print("Full list of Superpowers ", extended_superpowers)
+
+superpower_test = input("Please enter Superpower to test rating: ")
+superpower_test_value = extended_superpowers[superpower_test]
+
+if superpower_test_value > SUPERPOWER_UPPER_LIMIT:
+    print("SUPER COOL")
+else:
+    if SUPERPOWER_LOWER_LIMIT < superpower_test_value < SUPERPOWER_UPPER_LIMIT:
+        print("COOL")
