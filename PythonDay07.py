49a525a4996c5774f4e242631263616b0ef5758d
MarkSLegion <markshipsides@yahoo.co.uk>
2021-04-26 20:27:05 +0100

    Version 02

--- PythonDay07.py
+++ PythonDay07.py
@@ -1,6 +1,11 @@
 # MarkS - Python Challenge Day 07 -  SlackAdder
 
 
+def integer_list(number_string):
+    integer_stripped = [int(x) for x in number_string.split(',')]
+    return integer_stripped
+
+
 def slack_adder(integer_numbers, target_integer):
     list_length = len(integer_numbers)
     for i in range(list_length):
@@ -9,7 +14,7 @@ def slack_adder(integer_numbers, target_integer):
                 return i, j
 
 
-numbers: list = [11, 7, 2, 15]
-target: int = 9
+input_numbers = input("Please input a list of numbers: ")
+input_target = input("Please input a target sum: ")
 
-print(slack_adder(numbers, target))
+print(slack_adder(integer_list(input_numbers), int(input_target)))
