94cfedf096f18675a0e8e864e8f3ef04d330ea5c
MarkSLegion <markshipsides@yahoo.co.uk>
2021-04-27 15:30:48 +0100

    Version 02

--- PythonDay08Mod.py
+++ PythonDay08Mod.py
@@ -4,9 +4,9 @@ from typing import List
 BASIC_SUPERPOWERS: dict = {"Flight": 51, "Speed": 90, "Strength": 50, "Sight": 30}
 MY_WALLET: dict = {"USD": 170, "GBP": 50, "EUR": 30, "YEN": 50}
 
-MeetingRatings = List[str]
+ratings = List[str]
 
-def meet_ratings(rating_dict: dict, rating_minimum: int) -> MeetingRatings:
+def meet_ratings(rating_dict: dict, rating_minimum: int) -> ratings:
     rating_list: list = []
     for rating_name, rating_value in rating_dict.items():
         if rating_value >= rating_minimum:
