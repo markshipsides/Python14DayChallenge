6882233b2a562d5e41c3392c9259d3cbef212201
MarkSLegion <markshipsides@yahoo.co.uk>
2021-04-27 14:34:36 +0100

    Version 02

--- PythonDay08.py
+++ PythonDay08.py
@@ -4,9 +4,7 @@ BASIC_SUPERPOWERS: dict = {"Flight": 51, "Speed": 90, "Strength": 50, "Sight": 3
 MY_WALLET: dict = {"USD": 170, "GBP": 50, "EUR": 30, "YEN": 50}
 
 
-def meet_ratings(rating_dict, rating_minimum):
-    rating_name: str
-    rating_value: int
+def meet_ratings(rating_dict: dict, rating_minimum: int) -> list:
     rating_list: list = []
     for rating_name, rating_value in rating_dict.items():
         if rating_value >= rating_minimum:
@@ -14,10 +12,8 @@ def meet_ratings(rating_dict, rating_minimum):
     return rating_list
 
 
-def amount_spent(wallet, currency, currency_spent):
-    currency_available: int
-    currency_spent: int
-    currency_available = wallet.get(currency, 0)
+def amount_spent(wallet: dict, currency: str, currency_spent: int) -> int:
+    currency_available: int = wallet.get(currency, 0)
     if currency_available > currency_spent:
         currency_available = currency_spent
     return currency_available
