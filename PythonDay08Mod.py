9cf398891b732aca21b00f2416a81b1ebb3f3d57
MarkSLegion <markshipsides@yahoo.co.uk>
2021-04-27 15:22:37 +0100

    Version 02

--- PythonDay08Mod.py
+++ PythonDay08Mod.py
@@ -1,9 +1,12 @@
 # MarkS - Python Challenge Day 08Main - Re-usability
+from typing import List
 
 BASIC_SUPERPOWERS: dict = {"Flight": 51, "Speed": 90, "Strength": 50, "Sight": 30}
 MY_WALLET: dict = {"USD": 170, "GBP": 50, "EUR": 30, "YEN": 50}
 
-def meet_ratings(rating_dict: dict, rating_minimum: int) -> list:
+MeetingRatings = List[str]
+
+def meet_ratings(rating_dict: dict, rating_minimum: int) -> MeetingRatings:
     rating_list: list = []
     for rating_name, rating_value in rating_dict.items():
         if rating_value >= rating_minimum:
