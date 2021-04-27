e48b99cac5afc6e291fff88493335431f9e6f96c
MarkSLegion <markshipsides@yahoo.co.uk>
2021-04-27 16:21:16 +0100

    Version 02

--- PythonDay09.py
+++ PythonDay09.py
@@ -1,1 +1,15 @@
 # MarkS - Python Challenge Day 09 - Exceptions
+from PythonDay08Mod import *
+
+
+while True:
+    try:
+        minimum_rating = int(input("Please Enter Minimum rating value between 0-100: "))
+        break
+    except ValueError:
+        print("Not an integer")
+
+print(meet_ratings(BASIC_SUPERPOWERS, minimum_rating))
+chosen_currency = input("Please Select a currency: ")
+currency_amount = int(input("Please choose an amount to spend: "))
+print(amount_spent(MY_WALLET, chosen_currency, currency_amount))
