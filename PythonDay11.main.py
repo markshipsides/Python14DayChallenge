61ff5342b05c6737a24421fbdbbe597be0b62db8
MarkSLegion <markshipsides@yahoo.co.uk>
2021-04-29 12:36:50 +0100

    ma

--- PythonDay11.main.py
+++ PythonDay11.main.py
@@ -4,4 +4,3 @@ from PythonDay11mod import *
 mark_wallet = Wallet()
 mark_wallet.name = "Marks International Wallet"
 mark_wallet.balances = {"GBP": 100, "EUR": 80}
-new_gbp_balance = mark_wallet.spend_money("GBP",50)
--- PythonDay11mod.py
+++ PythonDay11mod.py
@@ -15,3 +15,10 @@ def spend_money(self, currency: str, amount: int) -> int:
 
 def display_balances(self):
     print(self.balances)
+
+
+mark_wallet = Wallet()
+mark_wallet.name = "Marks International Wallet"
+mark_wallet.balances = {"GBP": 100, "EUR": 80}
+
+new_gbp_balance = mark_wallet.spend_money("GBP", 50)
