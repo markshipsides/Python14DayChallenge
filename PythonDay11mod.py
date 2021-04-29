9452d2a3159edd2d161fbe8550a95e93478524af
MarkSLegion <markshipsides@yahoo.co.uk>
2021-04-29 13:03:57 +0100

    ma

--- PythonDay11mod.py
+++ PythonDay11mod.py
@@ -7,18 +7,15 @@ class Wallet:
     name: str
     balances: Dict[str, int]
 
+    def spend_money(self, currency: str, amount: int) -> int:
+        self.balances[currency] = self.balances.get(currency, 0) - amount
+        return self.balances[currency]
 
-def spend_money(self, currency: str, amount: int) -> int:
-    self.balances[currency] = self.balances[currency].get(currency, 0) - amount
-    return self.balances[currency]
-
-
-def display_balances(self):
-    print(self.balances)
+    def display_balances(self):
+        print(self.balances)
 
 
 mark_wallet = Wallet()
 mark_wallet.name = "Marks International Wallet"
 mark_wallet.balances = {"GBP": 100, "EUR": 80}
-
 new_gbp_balance = mark_wallet.spend_money("GBP", 50)
