a2edacca11c6e4d2f138d02a6f122a158d66f8d2
MarkSLegion <markshipsides@yahoo.co.uk>
2021-04-29 11:48:22 +0100

    Version 02

---
+++ PythonDay11.main.py
@@ -0,0 +1,9 @@
+# MarkS - Python Challenge Day 11main - Object-oriented programming
+from PythonDay11mod import *
+
+minimum_rating = int(input("Please Enter Minimum rating value: "))
+print(meet_ratings(BASIC_SUPERPOWERS, minimum_rating))
+
+chosen_currency = input("Please Select a currency: ")
+currency_amount = int(input("Please choose an amount to spend: "))
+print(amount_spent(MY_WALLET, chosen_currency, currency_amount))
