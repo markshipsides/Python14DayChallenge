f44e753974ff54fc4fe3258ab696c9db970d92ec
MarkSLegion <markshipsides@yahoo.co.uk>
2021-04-30 12:40:08 +0100

    version 01

--- PythonDay11.main.py
+++ PythonDay11.main.py
@@ -1,6 +1,10 @@
 # MarkS - Python Challenge Day 11main - Object-oriented programming
 from PythonDay11mod import *
 
-mark_wallet = Wallet()
-mark_wallet.name = "Marks International Wallet"
-mark_wallet.balances = {"GBP": 100, "EUR": 80}
+mark_wallet = Wallet("Marks International Wallet", {"USD": 0, "GBP": 0, "EUR": 0, "YEN": 0})
+peter_wallet = Wallet("Peter Wallet", {"USD": 0, "GBP": 0, "EUR": 0, "YEN": 0})
+mark_wallet.deposit_money("GBP", 375)
+mark_wallet.spend_money("GBP", 100)
+mark_wallet.transfer_money(peter_wallet, "GBP", 50)
+mark_wallet.display_balances()
+peter_wallet.display_balances()
--- PythonDay11mod.py
+++ PythonDay11mod.py
@@ -27,12 +27,3 @@ class Wallet:
 
     def display_balances(self):
         print(self.balances)
-
-
-mark_wallet = Wallet("Marks International Wallet", {"USD": 0, "GBP": 0, "EUR": 0, "YEN": 0})
-peter_wallet = Wallet("Peter Wallet", {"USD": 0, "GBP": 0, "EUR": 0, "YEN": 0})
-mark_wallet.deposit_money("GBP", 375)
-mark_wallet.spend_money("GBP", 100)
-mark_wallet.transfer_money(peter_wallet, "GBP", 50)
-mark_wallet.display_balances()
-peter_wallet.display_balances()
